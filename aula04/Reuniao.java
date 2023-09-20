912a94ab93e289bf5996f82f8addd3bea2dbfe03
mmamorim <marc.m.amorim@gmail.com>
2023-09-19 21:18:01 -0300

    v3

--- aula04/Reuniao.java
+++ aula04/Reuniao.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Reuniao {
     public static void main(String args[]) {
         int horaI, minI, horaF, minF;
+        int tempoI, tempoF, duracaoH, duracaoM;
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("Digite Horas início reunião: ");
@@ -16,6 +17,11 @@ public class Reuniao {
         System.out.println("Digite Minutos término reunião: ");
         minF = leitor.nextInt();
 
+        tempoI = horaI * 60 + minI;
+        tempoF = horaF * 60 + minF;
         
+        duracaoH = (tempoF-tempoI) / 60;
+        duracaoM = (tempoF-tempoI) % 60;
+        System.out.println("Duração: "+duracaoH+":"+duracaoM);
     }
 }
