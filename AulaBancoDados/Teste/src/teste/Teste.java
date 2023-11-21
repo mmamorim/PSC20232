14a36922692272aac82cf52088a42802f2182b3e
mmamorim <marc.m.amorim@gmail.com>
2023-11-21 19:24:34 -0300

    v2111

--- AulaBancoDados/Teste/src/teste/Teste.java
+++ AulaBancoDados/Teste/src/teste/Teste.java
@@ -1,47 +1,23 @@
 package teste;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import teste.User.UserCrud;
 
 public class Teste {
 
     public static void main(String[] args) {
-        Connection connection = null;
-        try
-        {
-          // create a database connection
-          connection = DriverManager.getConnection("jdbc:sqlite:banco.db");
-          Statement statement = connection.createStatement();
-          statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-          ResultSet rs = statement.executeQuery("select * from Usuarios");
-          while(rs.next())
-          {
+        UserCrud crud = new UserCrud();
+        crud.setVisible(true);
+        
+        DB db = new DB("banco.db");
+        db.query("select * from Usuarios");
+        while(db.next())
+        {
             // read the result set
-            System.out.println("codigo = " + rs.getString("codigo"));
-            System.out.println("nome = " + rs.getString("nome"));
-          }
+            System.out.println("codigo = " + db.getString("codigo"));
+            System.out.println("nome = " + db.getString("nome"));
+            System.out.println("email = " + db.getString("email"));
         }
-        catch(SQLException e)
-        {
-          // if the error message is "out of memory",
-          // it probably means no database file is found
-          System.err.println(e.getMessage());
-        }
-        finally
-        {
-          try
-          {
-            if(connection != null)
-              connection.close();
-          }
-          catch(SQLException e)
-          {
-            // connection close failed.
-            System.err.println(e.getMessage());
-          }
-        }
+        db.closeConnection();
     }
 }
